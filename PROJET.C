87a7e7ffc1f3095c27e937bb66caeca94e20758b
b a b a <50801068+babristor@users.noreply.github.com>
2024-08-15 18:06:32 +0100

    Add files via upload

---
+++ PROJET.C
@@ -0,0 +1,18 @@
+/************************************/
+/*   		youssef baba            */
+/*   		mesnawi@me.com          */
+/*	https://twitter.com/babristor   */
+/************************************/
+
+#include"projet.h"
+
+int a,b;
+
+void main()
+{
+	ini_graph();
+
+	setbkcolor(8);
+ 
+ 	page_0(&a,&b);
+}
